1a3f4f30f14a7281fca615c27585e9bd4f121bcb
Ray Ng <rng@frgteam.com>
2014-02-27 17:04:06 +1100

    Version fix attempt 1

--- tasks/mstest.js
+++ tasks/mstest.js
@@ -9,7 +9,13 @@
 'use strict';
 var exec = require('child_process').exec;
 var Path = require('path');
-
+var versions = {
+        1.0: '1.0.3705',
+        1.1: '1.1.4322',
+        2.0: '2.0.50727',
+        3.5: '3.5',
+        4.0: '4.0.30319'
+    };
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
@@ -22,6 +28,8 @@ module.exports = function(grunt) {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       mstestPath: "\"\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\IDE\\MSTest.exe\"",
+	  processor: '',
+	  version: 4.0,
     });
 
     // Iterate over all specified file groups.
@@ -29,7 +37,7 @@ module.exports = function(grunt) {
       return "/testcontainer:"+filePath;
     }).join(" ");
 
-    var child = exec(options.mstestPath + " " +containerString ,function (error, stdout, stderr) {
+    var child = exec(getBuildExecutablePath(options.version, options.processor) + " " +containerString ,function (error, stdout, stderr) {
         grunt.log.writeln(stdout);
 
         if(!stderr && stderr !== ""){
@@ -42,5 +50,33 @@ module.exports = function(grunt) {
         done();
     });
   });
+  
+ 
+  function getBuildExecutablePath(version, processor) {
+
+        // temp mono xbuild hack for linux / osx - assumes xbuild is in the path, works on my machine (Ubuntu 12.04 with Mono JIT compiler version 3.2.1 (Debian 3.2.1+dfsg-1~pre2))
+        if (process.platform === 'linux' || process.platform === 'darwin') {
+            return 'xbuild';
+        }
+
+        processor = 'Framework' + (processor === 64 ? processor : '');
+
+        version = versions[version];
+
+        if (!version) {
+            grunt.fatal('Unrecognised .NET framework version "' + version + '"');
+        }
+
+        var buildExecutablePath = path.join(process.env.WINDIR, 'Microsoft.Net', processor, 'v' + version, 'MSBuild.exe');
+
+        if (!fs.existsSync(buildExecutablePath)) {
+            grunt.fatal('Unable to find MSBuild executable');
+        }
+
+        return buildExecutablePath;
+
+    }
 
 };
+
+
