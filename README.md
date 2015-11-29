92d3714711af5848a78a38de4d03231eb3af5e9c
M.Scheepers <mscheepers@users.noreply.github.com>
2015-11-29 17:13:43 +0100

    Revert "Feature improvement"

--- README.md
+++ README.md
@@ -19,55 +19,7 @@ grunt.loadNpmTasks('grunt-mstest');
 
 ## The "mstest" task
 
-Run this task with the `grunt mstest` command.
-
-Task files and options may be specified according to the grunt [Configuring tasks](http://gruntjs.com/configuring-tasks) guide.
-
-### Options
-##### category
-Type: `String`
-
-Specify and filter which test categories to run (mstest /category).
-
-##### settings
-Type: `String`
-
-Use the specified test settings file (mstest /testsettings).
-
-##### results
-Type: `String`
-
-Save the test run results to the specified file (mstest /resultsfile).
-
-##### details
-Type: `Array` (defaults to `["errormessage", "errorstacktrace"]`)
-
-Specify the name of a property that you want to show values for, if any, in addition to the test outcome (mstest /detail).
-
-##### usestderr
-Type: `Boolean` (defaults to `true`)
-
-Use standard error to output error information (mstest /usestderr).
-
-##### nologo
-Type: `Boolean` (defaults to `true`)
-
-Display no startup banner and copyright message (mstest /nologo).
-
-
-##### executable
-Type: `String`
-
-The path to the mstest executable. If omitted this task tries to find the executable for VS100, VS110 and VS120. If no executable could be found this task will fail.
-
-##### mstestPath
-Type: `String` (deprecated: replaced by `executable`)
-
-The path to the mstest executable.
-
 ###Example
-
-Usage with just a test library
 
 ```javascript
 mstest: {
@@ -77,26 +29,6 @@ mstest: {
 }
 ```
 
-Usage with unit and integration tests identified by categories
-```javascript
-mstest: {
-    unittests: {
-        src: ['tmp/*.dll],          // points to test dll
-        options: {
-            category: 'Unit Tests'  // executes only tests decorated with the TestCategory("Unit Tests") attribute.
-        }
-    },
-    integrationtests: {
-        src: ['tmp/*.dll'],
-        options: {
-            force: true,                    // integration tests may fail without failing the build
-            category: 'Integration Tests'   // executes only tests decorated with the TestCategory("Integration Tests") attribute.
-        }
-    }
-}
-```
-
-
 ### Forcing tests
 You can add force:true to the options to prevent test failures resulting in a build failure, useful for CI workflow.
 
@@ -105,7 +37,6 @@ You can add force:true to the options to prevent test failures resulting in a bu
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
-- 0.1.5 Add support for additional mstest command line arguments
 - 0.1.4 Add force feature
 - 0.1.3 Change priority of vstools to newest first
 - 0.1.2 Fix issue where vstools 10 or 12 was not detected
--- tasks/mstest.js
+++ tasks/mstest.js
@@ -2,199 +2,93 @@
 * grunt-mstest
 * https://github.com/mrjackdavis/grunt-mstest
 *
-* Copyright (c) 2014-2015 mrjackdavis mscheepers
+* Copyright (c) 2014 mrjackdavis
 * Licensed under the MIT license.
 */
 
-/*jslint white:true vars:true plusplus:true*/
-/*global module, require, process*/
+'use strict';
+var exec = require('child_process').exec;
+var path = require('path');
+var fs = require('fs');
+
 module.exports = function(grunt) {
-    'use strict';
 
-    // extend string prototype and add method to quote strings
-    // yes, I am a lazy typer
-    if (!String.prototype.quotes) {
-        String.prototype.quotes = function() {
-            return '"' + this + '"';
-        };
-    }
+// Please see the Grunt documentation for more information regarding task
+// creation: http://gruntjs.com/creating-tasks
 
-    // documentation https://nodejs.org/api/fs.html#fs_fs_existssync_path
-    // tells us this method will be deprecated, thus if that method is not
-    // available we implement it by the use of statSync encapsulated
-    // with try catch to determine whether a path exists.
-    var fs = require('fs');
+    grunt.registerMultiTask('mstest', 'The best mstest Grunt plugin ever.', function() {
 
-    if (!fs.existsSync) {
-        grunt.log.debug('adding method fs.existsSync');
-        fs.existsSync = function(path) {
-            var result = true;
-            try {
-                this.statSync(path);
-            } catch (err) {
-                grunt.log.debug(err);
-                result = false;
-            }
-            return result;
-        };
-    }
+        var done = this.async();
 
-    // switched to span to gain performance due to streaming support
-    var spawn = require('child_process').spawn,
-        path = require('path');
-        
-    // will search for an appropriate mstest.exe if no executable was specified
-    function findExecutable() {
-        // environment variables for visual studio tools paths
-        var paths = [process.env.VS100COMNTOOLS, process.env.VS110COMNTOOLS, process.env.VS120COMNTOOLS, process.env.VS140COMNTOOLS],
-            executable = null,
-            notools = true,
-            p = null,
-            i = 0;
-
-        // check each path and try to find the MSText.exe
-        for(i = 0; i < paths.length; i++) {
-            p = paths[i];
-            if (p && p !== "") {
-                if (notools) {
-                    notools = false;
-                }
-                var temp = path.join(p, '../IDE', 'MSTest.exe');
-                if (fs.existsSync(temp)) {
-                    if (!executable) {
-                        grunt.log.debug("using MSTest.exe at " + path.join(p, '../IDE'));
-                    }
-                    else {
-                        grunt.log.debug("switching to more recent version at " + path.join(p, '../IDE'));
-                    }
-                    executable = temp;
-                }
-            }
-        }
-
-        if(notools) {
-            grunt.fatal("Neither Visual Studio Tools where found (checked VS100, VS110, VS120).");
-        }
-
-        if (!executable) {
-            grunt.fatal("Unable to find MSTest.exe. Use the 'executable' option to override");
-        }
-
-        return executable;
-    }
-
-    // returns the specified path to the mstest command. if none was
-    // specified findExecutable() looks for it
-    function composeCommand(options) {
-        // compose the command
-        var result = options.executable || options.mstestPath;
-        if (!result) {
-            result = findExecutable();
-        }
-        return result;
-    }
-
-    // composes an argument array populated with the MsTest.exe arguments
-    // according to the specified options
-    function composeArguments(files, options) {
-        // compose the arguments
-        var result = [],
-            i = 0;
-
-        // add a test container for each source
-        files.map(function(filepath) {
-            result.push('/testcontainer:' + filepath.quotes());
-        });
-
-        // add category if specified
-        if (options.category && options.category !== "") {
-            result.push('/category:' + options.category.quotes());
-        }
-
-        // add test settings if specified
-        if (options.settings && options.settings !== "") {
-            result.push('/testsettings:' + options.settings.quotes());
-        }
-
-        // add result file if specified
-        if (options.results && options.results !== "") {
-            result.push('/resultsfile:' + options.results.quotes());
-        }
-
-        // add details if specified
-        // NOTE: don't quote the property ids otherwise mstest can't find them
-        for(i = 0; i < options.details.length; i++) {
-            result.push('/detail:' + options.details[i]);
-        }
-
-        // add nologo if specified
-        if (options.nologo) {
-            result.push('/nologo');
-        }
-        
-        // add usestderr if specified
-        if (options.usestderr) {
-            result.push('/usestderr');
-        }
-        
-        return result;
-    }
-
-    // Please see the Grunt documentation for more information regarding task
-    // creation: http://gruntjs.com/creating-tasks
-    grunt.registerMultiTask('mstest', 'Executes Visual Studio test projects', function() {
-
-        // initialize default options and start asynchronously
+        // Merge task-specific and/or target-specific options with these defaults.
         var options = this.options({
-            category: null,
-            settings: null,
-            results: null,
-            details: ["errormessage", "errorstacktrace"],
-            usestderr: true,
-            nologo: true,
-            executable: null,
-            mstestPath: null,       // deprecated (renamed to executable)
-            force: false
-        }),
-            cmd = null,
-            args = null,
-            done = this.async();
-
-        grunt.log.debug('options: ' + JSON.stringify(options, null, 4));
-        
-        if (this.filesSrc.length === 0) {
-            grunt.fatal('At least one test container must be specified');
-            done(false);
-        }
-        
-        cmd = composeCommand(options);
-        args = composeArguments(this.filesSrc, options);
-        
-        grunt.log.debug('command: ' + cmd);
-        grunt.log.debug('arguments: ' + args);
-        
-        var cp = spawn(cmd, args, {stdio: 'inherit'});
-        
-        // handle general error (e.g. ENOENT)
-        cp.on('error', function(err) {
-            grunt.fatal(err);
-            done(false);
+            mstestPath: getExePath(),
+            details:["errormessage","errorstacktrace"],
+            force:false
         });
-        
-        // handle child process exit result and force option
-        cp.on('exit', function(code, signal) {
-            grunt.log.debug('close received - code: ' + code + ' signal: ' + signal);
-            if (code !== 0) {
-                // recognize configuration options: { force:true } _and_ grunt command line -f --force
-                if (options.force || grunt.option('force')) {
-                    grunt.log.warn("Even though tests failed execution is continued");
-                }
-                else {
-                    grunt.warn("Test run failed");
-                    done(false);
-                }
-            } 
+
+        function gruntWarn(str){
+            if(options.force)
+                grunt.log.writeln(str);
+            else
+                grunt.fail.warn(str);
+        }
+
+        // Iterate over all specified file groups.
+        var containerString = this.filesSrc.map(function(filePath){
+            return "/testcontainer:"+filePath;
+        }).join(" ");
+
+        for (var i = options.details.length - 1; i >= 0; i--) {
+            containerString += " /detail:"+options.details[i]
+        };
+
+        containerString +=" /usestderr";
+
+        var child = exec(escapeShell(options.mstestPath) +containerString ,function (error, stdout, stderr) {
+            grunt.log.writeln(stdout);
+
+            if(!stderr && stderr !== ""){
+                gruntWarn("stderr:\""+stderr+"\"",3);
+            }
+            if (error !== null) {
+                gruntWarn(error,3);
+            }
+
             done();
         });
     });
+
+
+    function getExePath() {
+        //Possible env variables for visual studio tools, in reverse order of priority
+        var vsToolsArr = [process.env.VS100COMNTOOLS,process.env.VS110COMNTOOLS,process.env.VS120COMNTOOLS]
+
+        //Get highest priority VS tools
+        var vsTools = null;
+        for (var i = vsToolsArr.length - 1; i >= 0; i--) {
+            var item = vsToolsArr[i]
+            if(item && item != ""){
+                vsTools = item;
+                break;
+            }
+        };
+
+        if(!vsTools)
+            grunt.fatal("Visual studio tools not installed")
+
+        var exePath = path.join(vsTools, "../IDE", 'MSTest.exe');
+
+        if (!fs.existsSync(exePath)) {
+            grunt.fatal('Unable to find MSTest executable at '+exePath +" use the option 'mstestPath' to override");
+        }
+
+        return exePath;
+    }
+
+    function escapeShell(cmd) {
+      return '"'+cmd+'"';
+    };
 };
+
+
